75b78f70b5a60b7b0d14a16759b925556ce1abf4
enesTekneci <enes.tekneci8181@gmail.com>
2024-10-12 18:49:53 +0300

    Login deneme

---
+++ lib/pages/login_register_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../auth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String? errorMessage = '';
+  bool isLogin = true;
+
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      await Auth().signInWihEmailAndPassword(email: _controllerEmail.text, password: _controllerPassword.text);
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Future<void> createUsersWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWihEmailAndPassword(email: _controllerEmail.text, password: _controllerPassword.text);
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Widget _title() {
+    return const Text('NewZ Login or Register');
+  }
+
+  Widget _entryField(
+    String title,
+    TextEditingController controller,
+  ) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: title,
+      ),
+    );
+  }
+
+  Widget _errorMessage() {
+    return Text(errorMessage == '' ? '' : '$errorMessage');
+  }
+
+  Widget _submitButton(){
+    return ElevatedButton(onPressed: isLogin ? signInWithEmailAndPassword : createUsersWithEmailAndPassword, child: Text(isLogin ? 'Login' : 'Register'),);
+  }
+
+  Widget _loginOrRegisterButton(){
+    return TextButton(onPressed: () {
+      setState(() {
+        isLogin = !isLogin;
+      });
+    }, child: Text(isLogin ? "Register Instead": "Login Instead"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _title(),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            _entryField('Email', _controllerEmail),
+            _entryField('Password', _controllerPassword),
+            _errorMessage(),
+            _submitButton(),
+            _loginOrRegisterButton(),
+          ],
+        ),
+      ),
+    );
+  }
+} 
